4013498e11f26f3ca1640ce002f049df868a8ea3
David Thornton <northdot9@gmail.com>
2018-01-14 00:19:20 -0500

    versions is important

---
+++ versions.py
@@ -0,0 +1,20 @@
+import sys
+print("Python version: %s" % sys.version)
+
+import pandas as pd
+print("pandas version: %s" % pd.__version__)
+
+import matplotlib
+print("matplotlib version: %s" % matplotlib.__version__)
+
+import numpy as np
+print("NumPy version: %s" % np.__version__)
+
+import scipy as sp
+print("SciPy version: %s" % sp.__version__)
+
+import IPython
+print("IPython version: %s" % IPython.__version__)
+
+import sklearn
+print("scikit-learn version: %s" % sklearn.__version__)
